78bf6d76285124c57bf1db8fc9e56d45ac798cb0
gustavo2892 <gustavo2892@gmail.com>
2022-06-28 22:10:30 -0300

    CSS modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+import { Header } from "./components/Header/Header";
+
 function App() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <h1>Hello World</h1>
+      <Header/>
+    </>
   )
 };
 
